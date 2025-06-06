efa854a450aaf15c705a1ab6c35bbeeb9f7f5ace
Eric Liu <eric.liu@defang.io>
2025-06-06 15:02:27 -0700

    add model mapping to aws embedding

--- src/api/setting.py
+++ src/api/setting.py
@@ -16,7 +16,7 @@ USE_MODEL_MAPPING = os.getenv("USE_MODEL_MAPPING", "true").lower() != "false"
 
 AWS_REGION = os.environ.get("AWS_REGION", "us-west-2")
 DEFAULT_MODEL = os.environ.get("DEFAULT_MODEL", "anthropic.claude-3-sonnet-20240229-v1:0")
-DEFAULT_EMBEDDING_MODEL = os.environ.get("DEFAULT_EMBEDDING_MODEL", "cohere.embed-multilingual-v3")
+DEFAULT_EMBEDDING_MODEL = os.environ.get("DEFAULT_EMBEDDING_MODEL", "embeddings-default")
 ENABLE_CROSS_REGION_INFERENCE = os.environ.get("ENABLE_CROSS_REGION_INFERENCE", "true").lower() != "false"
 
 GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
